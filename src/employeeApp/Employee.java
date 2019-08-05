954c251fbf02cfc0cbb8314eea208a89bc9def52
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-08-05 09:41:23 -0700

    Finished setting getters and setters for the employee object

--- src/employeeApp/Employee.java
+++ src/employeeApp/Employee.java
@@ -7,8 +7,8 @@ public class Employee {
     private String lname;
     private double salary;
     private boolean has401K;
-    private int company;
-    private int healthPlanId;
+    private int companyID;
+    private int healthPlanID;
 
     // Methods
 
@@ -17,4 +17,53 @@ public class Employee {
         return id;
     }
 
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean getHas401K()
+
+    {
+        return has401K;
+    }
+
+    public void setHas401K(boolean has401K) {
+        this.has401K = has401K;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+
+    public int getHealthPlanID() {
+        return healthPlanID;
+    }
+
+    public void setHealthPlanID(int healthPlanID) {
+        this.healthPlanID = healthPlanID;
+    }
 }
