41d025804b3cc47fabd33eb85dd04bf05fbd79a0
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-08-05 09:30:22 -0700

    Initialized project

---
+++ src/employeeApp/Employee.java
@@ -0,0 +1,20 @@
+package employeeApp;
+
+public class Employee {
+    // Objects Data fields or State
+    private int id;
+    private String fname;
+    private String lname;
+    private double salary;
+    private boolean has401K;
+    private int company;
+    private int healthPlanId;
+
+    // Methods
+
+    // Getters && Setters
+    public int getId() {
+        return id;
+    }
+
+}
